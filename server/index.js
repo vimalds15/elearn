a263b92f704382eae388d8b8e4cb2c916d411a18
vimalds15 <antovimalands@gmail.com>
2021-08-25 12:53:31 +0530

    basic routes creation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db")
+
+//connect database
+connectDB();
+
 
 app.listen(port,()=>{
     console.log(`Successfully running on port ${port}`)
